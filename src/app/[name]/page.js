357d0c4cf3923fa06d90812aba27de5a94fab2a4
Axel Sorensen <axelsorensenwork@gmail.com>
2023-04-22 17:12:56 +0200

    first commit

--- src/app/[name]/page.js
+++ src/app/[name]/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 export const dynamicParams = true 
+export const dynamic = 'force-dynamic'
 export default function Other() {
   return (
     <div>Hello other</div>
---
+++ src/app/folder/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export const dynamicParams = true;
+export const dynamic = 'force-dynamic'
+export default function Home() {
+  return (
+    <div>Normal folder works</div>
+  )
+}
