48e2ef29353abd5226b6a485409f02723a65d79b
Axel Sorensen <Axel123@live.dk>
2023-04-27 12:54:19 +0200

    pwa

--- next.config.js
+++ next.config.js
@@ -4,11 +4,14 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: true,
+  
 });
 
 const nextConfig = withPWA({
   output: "export",
-  assetPrefix: '/ResearchReader/',
+  basePath: '/ResearchReader',
+  assetPrefix: '/ResearchReader/'
 });
 
 module.exports = nextConfig;
